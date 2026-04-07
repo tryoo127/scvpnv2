bc7fea7b96c21b71fd433776fc4204ad51b12ede
tryoo127 <tryoo127@gmail.com>
2026-04-07 08:55:23 +0800

    Update ins-xray-deb10.sh

--- install/ins-xray-deb10.sh
+++ install/ins-xray-deb10.sh
@@ -25,7 +25,7 @@ touch /usr/local/etc/xray/akunxtr.conf
 latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
 
 # Installation Xray Core
-xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v1.7.5.1/xray-linux-64.zip"
+xraycore_link="https://github.com/NevermoreSSH/Xcore-custompath/releases/download/Xray-linux-64-v1.7.2-1/Xray-linux-64-v1.7.2-1"
 
 # Make Main Directory
 mkdir -p /usr/bin/xray
