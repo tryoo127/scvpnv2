868b92d7cac28c949cce35dd0823d58f5572f517
tryoo127 <tryoo127@gmail.com>
2026-04-07 10:52:06 +0800

    Update ins-xray-deb10.sh

--- install/ins-xray-deb10.sh
+++ install/ins-xray-deb10.sh
@@ -21,12 +21,22 @@ date
 mkdir -p /usr/local/etc/xray/
 touch /usr/local/etc/xray/akunxtr.conf
 
+# CHECK VERSION XRAY CORE
+latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+
 # Installation Xray Core
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/Xray-linux-64.zip"
 
 # Make Main Directory
 mkdir -p /usr/bin/xray
 mkdir -p /etc/xray
+
+# Unzip Xray Linux 64
+cd `mktemp -d`
+curl -sL "$xraycore_link" -o xray.zip
+unzip -q xray.zip && rm -rf xray.zip
+mv xray /usr/local/bin/xray
+chmod +x /usr/local/bin/xray
 
 # Make Folder XRay
 mkdir -p /var/log/xray/
